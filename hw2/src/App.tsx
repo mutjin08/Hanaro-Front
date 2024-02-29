4a57521f26767cfb8aff95222e0d05fc184fb367
mutjin08 <mutjin08@gmail.com>
2024-02-29 09:08:30 +0900

    24-02-29

--- hw2/src/App.tsx
+++ hw2/src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import Layout from './component/Layout';
@@ -10,16 +9,18 @@ import Home from './component/Home';
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
-  <div>
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Logon />}></Route>
-        <Route path="/album/list" element={<AlbumList />}></Route>
-        <Route path="/photo/list" element={<PhotoList />}></Route>
-        <Route path="/home" element={<Home />}></Route>
-      </Route>
-    </Routes>
-  </div>
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Logon />}></Route>
+          <Route path="/album/list" element={<AlbumList />}></Route>
+          <Route path="/photo/list" element={<PhotoList />}></Route>
+          <Route path="/home" element={<Home />}></Route>
+        </Route>
+      </Routes>
+    </div>
+  )
 }
 
 export default App;
